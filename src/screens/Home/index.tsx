e172444784053b056bee572bf953baafaf435cea
Wellington dos Santos <w_santos1993@outlook.com>
2022-09-14 00:06:00 -0300

    criada a langind page mobile e scroll horizontal

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,36 @@
-import React from 'react';
-import { View } from 'react-native';
+import logoImg from '../../assets/logo-nlw-esports.png';
+import { View, Image, FlatList } from 'react-native';
 
 import { styles } from './styles';
+import { Heading } from '../../components/Heading';
+import { GameCard } from '../../components/GameCard';
+import { GAMES } from '../../utils/games'
 
 export function Home() {
   return (
     <View style={styles.container}>
+      <Image 
+      source={logoImg}
+      style={styles.logo}
+      />
+
+      <Heading 
+      title="Encontre seu duo!"
+      subtitle="Selecione o game que deseja jogar..."
+      />
+
+      <FlatList
+      data={GAMES}
+      keyExtractor={item => item.id}
+      renderItem={({item, }) => (
+        <GameCard
+        data={item}
+        />
+      )}
+      showsHorizontalScrollIndicator={false}
+      horizontal
+      contentContainerStyle={styles.contentList}
+      />  
 
     </View>
   );
