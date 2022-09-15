c34aa73c19decc1e70f8f00f6bf1b398be75e4aa
Wellington dos Santos <w_santos1993@outlook.com>
2022-09-15 15:19:27 -0300

    buscando dados da api externa

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,20 @@
 import logoImg from '../../assets/logo-nlw-esports.png';
 import { View, Image, FlatList } from 'react-native';
+import { useEffect, useState } from "react";
 
 import { styles } from './styles';
 import { Heading } from '../../components/Heading';
-import { GameCard } from '../../components/GameCard';
+import { GameCard, GameCardProps } from '../../components/GameCard';
 import { GAMES } from '../../utils/games'
 
 export function Home() {
+  const [games, setGames] = useState<GameCardProps[]>([]);
+  useEffect(( ) => {
+    fetch('http://192.168.100.77:3333/games')
+    .then(response => response.json())
+    .then(data => setGames(data));
+  }, []);
+
   return (
     <View style={styles.container}>
       <Image 
@@ -20,7 +28,7 @@ export function Home() {
       />
 
       <FlatList
-      data={GAMES}
+      data={games}
       keyExtractor={item => item.id}
       renderItem={({item, }) => (
         <GameCard
